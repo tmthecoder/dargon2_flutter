1d31ef9d16b92505da2f0127430f4c03460ebde5
Tejas Mehta <tmthecoder@gmail.com>
2021-12-09 15:51:14 -0500

    create the main platform plugin

---
+++ dargon2_flutter/lib/dargon2_flutter.dart
@@ -0,0 +1,9 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dargon2_flutter;
+
+
+// TODO: Export any libraries intended for clients of this package.
+export 'package:dargon2_interface/dargon2_interface.dart';
+export 'package:dargon2_flutter_platform_interface/dargon2_flutter_platform.dart' hide DArgon2Platform;
